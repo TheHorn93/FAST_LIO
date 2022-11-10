214b27718d51c1289dcd4573b4104c9b1f18508c
Jan Quenzel <jquenzel@ais.uni-bonn.de>
2022-11-10 12:03:17 +0100

    more reasonable params for subscriber.

--- src/laserMapping.cpp
+++ src/laserMapping.cpp
@@ -870,9 +870,9 @@ int main(int argc, char** argv)
 
     /*** ROS subscribe initialization ***/
     ros::Subscriber sub_pcl = p_pre->lidar_type == AVIA ? \
-        nh.subscribe(lid_topic, 200000, livox_pcl_cbk) : \
-        nh.subscribe(lid_topic, 200000, standard_pcl_cbk);
-    ros::Subscriber sub_imu = nh.subscribe(imu_topic, 200000, imu_cbk);
+        nh.subscribe(lid_topic, 10, livox_pcl_cbk) : \
+        nh.subscribe(lid_topic, 2, standard_pcl_cbk);
+    ros::Subscriber sub_imu = nh.subscribe(imu_topic, 50, imu_cbk);
     ros::Publisher pubLaserCloudFull = nh.advertise<sensor_msgs::PointCloud2>
             ("/cloud_registered", 100000);
     ros::Publisher pubLaserCloudFull_body = nh.advertise<sensor_msgs::PointCloud2>
