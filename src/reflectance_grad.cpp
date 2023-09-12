389f20ee52c05d4d6222ae72ce132debae3d11c0
Horn <s6jshorn@uni-bonn.de>
2023-09-12 17:48:33 +0200

    Add full grad normalization

--- src/reflectance_grad.cpp
+++ src/reflectance_grad.cpp
@@ -115,9 +115,10 @@ double IrregularGrid::call( const PointType& pt, const PointVector& pts_near, co
   Eigen::Matrix<double, 3, 2> mat_grad = getGradientMat( lambda );
   Eigen::Vector3d grad_on_plane = getIntensityGradOnPlane( pt_pos_rot, mat_grad );
 
-  double grad_norm = grad_on_plane.norm();
+  //double grad_norm = grad_on_plane.norm();
   //std::cout << "grad_n" << grad_norm << " <- " << grad_on_plane.transpose()<< std::endl;
   grad_out = (rot_quad.conjugate() *grad_on_plane).transpose() * (Eigen::Matrix<double, 3,3>::Identity() - normal * normal.transpose());
+  double grad_norm = grad_out.norm();
   if( grad_norm > 0.0 )
     grad_out /= grad_norm;
 
